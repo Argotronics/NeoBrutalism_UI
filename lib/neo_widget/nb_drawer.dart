28509f69a517239e9edba2f1e76e8afc13857a42
Austin <130708301+Argotronics@users.noreply.github.com>
2024-07-04 23:48:00 -0500

    added drawer

--- lib/neo_widget/nb_drawer.dart
+++ lib/neo_widget/nb_drawer.dart
@@ -1,3 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:rentals/neo_widget/neo_settings.dart';
 
-class NBDrawer extends -
+class NBDrawer extends StatefulWidget {
+  /// A customizable neobrutalist-style Drawer Template, allowing
+  /// user to experiment freely without any restriction in functionality.
+  ///
+  /// This button is built using Flutter's `Material` widget, and is designed to
+  /// follow the Neubrutalism UI style guidelines.
+  ///
+  /// *[Constants]
+
+  ///   - const neuBlack = Colors.black;
+  ///   - const neuDefault1 = Colors.teal;
+  ///   - const neuShadow = Color(0xFF080808);
+  ///
+  ///   - const neuBorder = 3.0;
+  ///   - const neuShadowBlurRadius = 0.0;
+  ///
+  ///   - const neuOffset = Offset(4, 4);
+  ///   - const neuBlurStyle = BlurStyle.solid;
+
+  const NBDrawer({
+    super.key,
+    this.offset = neoOffset,
+    this.borderStyle,
+    this.boxShape,
+    this.children,
+  });
+  final List<Widget>? children;
+
+  final BoxShape? boxShape;
+
+  /// - borderStyle (optional): A BorderStyle that defines the style of the border of the container.
+  ///
+  /// By default, it is set to BorderStyle.solid.
+  ///
+  /// if the boxShape is set to BoxShape.circle, the borderStyle will be ignored.
+  /// if the boxShape is set to BoxShape.rectangle, the borderStyle will be used.
+  /// if the boxShape is set to null, the borderStyle will be used.
+
+  final BorderStyle? borderStyle;
+
+  /// - offset (optional): An Offset that defines the position of the shadow of the container.
+  ///
+  /// By default, it is set to neoOffset.
+  ///  - offset (optional): An Offset that defines the position of the shadow of the container.
+
+  final Offset offset;
+
+  get borderRadius => null;
+
+  get borderColor => null;
+
+  get borderWidth => null;
+
+  get shadowColor => null;
+
+  get shadowBlurRadius => null;
+
+  get shadowBlurStyle => null;
+
+  get child => null;
+
+  @override
+  _NBDrawerState createState() => _NBDrawerState();
+}
+
+class _NBDrawerState extends State<NBDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+        border: Border.all(
+          color: Colors.black,
+          width: 3,
+        ),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: widget.shadowBlurRadius ?? 0.0,
+            offset: neoOffset,
+            spreadRadius: 0.0,
+            blurStyle: widget.shadowBlurStyle ?? neoBlurStyle,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(22),
+          clipBehavior: Clip.antiAlias,
+          child: NavigationDrawer(
+              backgroundColor: Colors.transparent,
+              children: widget.children ?? [])),
+    );
+  }
+}
--- lib/src/test/test.dart
+++ lib/src/test/test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:neubrutalism_ui/neubrutalism_ui.dart';
+import 'package:rentals/neo_widget/nb_drawer.dart';
 import '../../neo_widget/nb_container.dart';
 import '../../neo_widget/nb_clippers.dart';
 
@@ -27,66 +28,78 @@ class _TestViewState extends State {
         title: const Text('Test'),
       ),
       backgroundColor: Colors.grey,
-      body: Column(
+      body: Stack(
+        alignment: Alignment.bottomCenter,
         children: [
-          NeuSearchBar(
-            borderRadius: BorderRadius.circular(5),
-            borderWidth: 2,
-            shadowBlurRadius: 5,
-            borderColor: Colors.black,
-            searchBarColor: Colors.white,
-            leadingIcon: const Icon(Icons.search),
+          Container(
+            height: 500,
+            width: 200,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+              child: const NBDrawer(
+                  children: [
+                    Text(
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                        'test'),
+                    Text('test'),
+                    Text('test'),
+                    Text('test')
+                  ],
+                  boxShape: BoxShape.rectangle,
+                  borderStyle: BorderStyle.solid,
+                  offset: const Offset(4, 4)),
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: NeuTextButton(
+          Column(
+            children: [
+              NeuSearchBar(
                 borderRadius: BorderRadius.circular(5),
                 borderWidth: 2,
-                buttonWidth: 200,
-                buttonHeight: 75,
-                buttonColor: Colors.lightBlueAccent,
-                animationDuration: 200,
-                enableAnimation: true,
-                onPressed: () {
-                  sex = 'True he is';
-                },
-                text: const Text(textAlign: TextAlign.center, 'contest')),
-          ),
-          NeuContainer(
-              borderRadius: BorderRadius.circular(5),
-              borderWidth: 2,
-              width: MediaQuery.of(context).size.width * 0.5,
-              height: 200,
-              child: Column(
-                children: [
-                  const Text(
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 20),
-                      'test'),
-                  Divider(
-                    color: Colors.black,
-                    thickness: 2,
-                    indent: MediaQuery.of(context).size.width * 0.01,
-                    endIndent: MediaQuery.of(context).size.width * 0.01,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          NeuIconButton(
-                              borderRadius: BorderRadius.circular(5),
-                              borderWidth: 2,
-                              buttonWidth: 100,
-                              buttonHeight: 50,
-                              buttonColor: Colors.lightBlueAccent,
-                              animationDuration: 200,
-                              enableAnimation: true,
-                              icon: const Icon(Icons.add),
-                              onPressed: () {}),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(children: [
+                shadowBlurRadius: 5,
+                borderColor: Colors.black,
+                searchBarColor: Colors.white,
+                leadingIcon: const Icon(Icons.search),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: NeuTextButton(
+                    borderRadius: BorderRadius.circular(5),
+                    borderWidth: 2,
+                    buttonWidth: 200,
+                    buttonHeight: 75,
+                    buttonColor: Colors.lightBlueAccent,
+                    animationDuration: 200,
+                    enableAnimation: true,
+                    onPressed: () {
+                      sex = 'True he is';
+                    },
+                    text: const Text(textAlign: TextAlign.center, 'contest')),
+              ),
+              NeuContainer(
+                  borderRadius: BorderRadius.circular(5),
+                  borderWidth: 2,
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: 200,
+                  child: Column(
+                    children: [
+                      const Text(
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20),
+                          'test'),
+                      Divider(
+                        color: Colors.black,
+                        thickness: 2,
+                        indent: MediaQuery.of(context).size.width * 0.01,
+                        endIndent: MediaQuery.of(context).size.width * 0.01,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
                               NeuIconButton(
                                   borderRadius: BorderRadius.circular(5),
                                   borderWidth: 2,
@@ -95,43 +108,58 @@ class _TestViewState extends State {
                                   buttonColor: Colors.lightBlueAccent,
                                   animationDuration: 200,
                                   enableAnimation: true,
-                                  icon:
-                                      const Icon(Icons.exposure_minus_1_sharp),
-                                  onPressed: () {
-                                    test ??= 0;
-                                    test = test! + 1;
-                                    debugPrint(test.toString());
-                                    setState(() {});
-                                  })
+                                  icon: const Icon(Icons.add),
+                                  onPressed: () {}),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(children: [
+                                  NeuIconButton(
+                                      borderRadius: BorderRadius.circular(5),
+                                      borderWidth: 2,
+                                      buttonWidth: 100,
+                                      buttonHeight: 50,
+                                      buttonColor: Colors.lightBlueAccent,
+                                      animationDuration: 200,
+                                      enableAnimation: true,
+                                      icon: const Icon(
+                                          Icons.exposure_minus_1_sharp),
+                                      onPressed: () {
+                                        test ??= 0;
+                                        test = test! + 1;
+                                        debugPrint(test.toString());
+                                        setState(() {});
+                                      })
+                                ]),
+                              )
                             ]),
-                          )
-                        ]),
-                  ),
-                ],
-              )),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                NeoContainer(
-                  borderRadius: BorderRadius.circular(10),
-                  height: 150,
-                  width: 150,
-                  rotation: test!.toDouble(),
-                  child: Text(test.toString()),
-                )
-              ],
-            ),
-            Column(
-              children: [
-                SizedBox(
-                  height: 200,
-                  width: 200,
+                      ),
+                    ],
+                  )),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    NeoContainer(
+                      borderRadius: BorderRadius.circular(10),
+                      height: 150,
+                      width: 150,
+                      rotation: test!.toDouble(),
+                      child: Text(test.toString()),
+                    )
+                  ],
                 ),
-              ],
-            ),
-          ]),
-          Container(),
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 200,
+                      width: 200,
+                    ),
+                  ],
+                ),
+              ]),
+              Container(),
+            ],
+          ),
         ],
       ),
     );
